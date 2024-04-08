1a67679b7a40379eae996e01d8b4a078449257af
Charlie Larson <clarson@lcs.on.ca>
2024-04-08 11:42:17 -0400

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Charlie Larson on 2024-04-08.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
