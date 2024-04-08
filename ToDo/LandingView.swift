4f0309806164c3747acd1f39ee866a875814cb3c
Charlie Larson <clarson@lcs.on.ca>
2024-04-08 11:52:49 -0400

    Made Groups.

---
+++ ToDo/LandingView.swift
@@ -0,0 +1,29 @@
+//
+//  LandingView.swift
+//  To-do List App
+//
+//  Created by Charlie Larson on 2024-04-08.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    
+   // Mark Stored Properties
+    
+    // Mark: Computed Properties
+    var body: some View {
+        NavigationView {
+            
+            VStack {
+                
+            }
+            .navigationTitle("To Do")
+            
+        }
+    }
+}
+
+#Preview {
+    LandingView()
+}
