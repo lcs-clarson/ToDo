ece285c1d26d07a98cdc00e48d185f93376de899
Charlie Larson <clarson@lcs.on.ca>
2024-04-10 11:44:09 -0400

    Made the scrollable list iterate over an array.

--- ToDo/Views/LandingView.swift
+++ ToDo/Views/LandingView.swift
@@ -17,17 +17,18 @@ struct LandingView: View {
    // The search text
     @State var searchText = ""
     
+    // The list of to-do items
+    @State var todos: [TodoItem] = exampleItems
+    
     // Mark: Computed Properties
     var body: some View {
         NavigationView {
             
             VStack {
                 
-                List {
+                List(todos) { todo in
                     
-                    ItemView(currentItem: firstItem)
-                    ItemView(currentItem: secondItem)
-                    ItemView(currentItem: thirdItem)
+                    ItemView(currentItem: todo)
 
                 }
                 .searchable(text: $searchText)
